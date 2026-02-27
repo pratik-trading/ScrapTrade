01a8e0477171c3a85f4cf2e411958163e3979783
Pratik Jadhav <pratikvj9370@gmail.com>
2026-02-27 23:17:15 +0530

    lotAPI updated V_1.6

--- frontend/src/services/lotAPI.js
+++ frontend/src/services/lotAPI.js
@@ -1,9 +1,4 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api',
-  withCredentials: true,
-});
+import api from './api';
 
 export const lotAPI = {
   getAll: (params) => api.get('/lots', { params }),
